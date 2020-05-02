03587d9e20d9544f5242d7d5ff2305cd47235200
nicolas david lopez <deivid_147-@hotmail.com>
2020-05-01 22:29:21 -0500

    version 1

---
+++ Assets/cambiosAlCubo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class cambiosAlCubo : MonoBehaviour
+{
+    public static int puntaje = 0;
+    public Text textoPuntaje;
+
+    public void cambiarArojo()
+    {
+
+        Debug.Log("cambiar a rojo");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void cambiarAverde()
+    {
+
+        Debug.Log("cambiar a rojo");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void matar()
+    {
+        //puntaje = puntaje + 1;
+        puntaje++;
+        textoPuntaje.text = puntaje.ToString();
+        Debug.Log("mi puntaje es " + puntaje);
+
+        Destroy(this.gameObject);
+        evaluarGanar(puntaje);
+    }
+
+
+    private void evaluarGanar(int marcador)
+    {
+        if (marcador >= 5)
+        {
+            Debug.Log("gané!!!!");
+          
+        }
+    }
+
+}
---
+++ Assets/scripts/InfoUser.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoUser : MonoBehaviour
+{
+    private string UrlLeer = "http://tadeolabhack.com:8081/test/Datos/ActNowPhp/leerActNow.php";
+
+
+
+    public Text TextoNombre;
+
+
+    public int Cedula = 1032496725;
+
+    public void obtenerInfo()
+    {
+        StartCoroutine(datos());
+    }
+
+    public IEnumerator datos()
+    {
+        string info = UrlLeer + "?cedu=" + Cedula;
+
+        WWW resultInfo = new WWW(info);
+
+        yield return resultInfo;
+
+        print(resultInfo.text);
+
+        TextoNombre.text = resultInfo.text;
+
+
+
+
+    }
+
+
+
+
+
+
+}
